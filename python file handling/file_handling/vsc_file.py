8a48f1c879ae79005f1178e218b2b7ceebf1dcee
gowtham-ef123 <gowthambscit525@gmail.com>
2026-06-27 15:42:46 +0530

    uploading practice files

--- python file handling/file_handling/vsc_file.py
+++
@@ -1,7 +0,0 @@
-import csv
-with open(r"C:\Users\AnudipCOA\Downloads\python file handling\file_handling\new1.csv","w",newline='') as file :
-    writer=csv.write(file)
-    writer.write(["ink,25"])
-    writer.writerow(["ballpen",10])
-
-     
